4c1666b23d0a2d2204b3f5464bdd57b85b51ddd7
Emma Gospodinova <emma.gospodinova@gmail.com>
2017-07-26 00:16:52 +0100

    errors: add diagnostic levels

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -1,7 +1,13 @@
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Level {
+    Info,
+    Debug,
+    Warn,
+    Error,
+    Fatal
 }
 
 #[derive(Copy, Clone, Debug)]
 pub struct Diagnostic {
+    pub level: Level
 }
